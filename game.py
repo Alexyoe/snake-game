d00072365504ba838cf8289b559f6d906aaf819c
Alex Yoesting <alexyoe@icloud.com>
2021-04-10 22:14:29 -0400

    Added high scores

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
-import curses, os, sys
+import curses, os, sys, time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint, choice, randrange
 from time import sleep
 
 curses.initscr()  # initialize screen
+start_time = time.time()
 window = curses.newwin(30, 60, 0, 0)  # Create a new window
 window.keypad(True)  # Turn on keypad
 curses.noecho()  # Turn off echo
@@ -29,14 +30,25 @@ fruit = [10, 25]
 fruitIcon = ["○", "Ω", "♥", "♦", "♣", "♠"]
 # Display the first fruit
 window.addch(fruit[0], fruit[1], choice(fruitIcon), curses.color_pair(1))
+f = open("./scores.txt", "r+")  # Open high scores
 
 while key != 27:  # While they Esc key is not being pressed
     window.border(0)
+    elapsed_time = time.time() - start_time
+    scores = f.readlines()
+    allScores = [x.strip("\n") for x in scores]
+    if len(allScores) != 0:
+        highscore = max([int(i) for i in allScores])
+
     # Display the score and the title
     window.addstr(0, 2, "Score: " + str(score) + " ")
-    window.addstr(0, 27, " Snake ")
-    window.addstr(29, 2, " By: Alex Yoesting ")
+    window.addstr(0, 23, " Snake Game ")
+    window.addstr(
+        0, 44, "Time:" + str(time.strftime("%M:%S", time.gmtime(elapsed_time)))
+    )
+    window.addstr(29, 20, " By: Alex Yoesting ")
     window.addstr(29, 44, " Next Point: " + str(randomColor) + " ")
+    window.addstr(29, 2, " High Score: " + str(highscore) + " ")
     # Makes the snake speed up as it eats more
     window.timeout(140 - (int(len(snake) / 5) + int(len(snake) / 10) % 120))
     # refreshes the screen and then waits for the user to hit a key
@@ -83,22 +95,7 @@ while key != 27:  # While they Esc key is not being pressed
     )  # Add a block to snakes tail
 
 
-def yes_or_no(question):
-    while "the answer is invalid":
-        reply = str(input(question + " (y/n): ")).lower().strip()
-        if reply[0] == "y":
-            return True
-        if reply[0] == "n":
-            return False
-
-
 curses.endwin()  # Close the window and ends the game
-print("\nScore: " + str(score))  # Prints the score on game end
-continueGame = yes_or_no("Woule you like to play again?")
-
-if continueGame == True:
-    sleep(1)
-    os.execv(sys.executable, sys.argv)
-
-# curses.endwin()  # Close the window and ends the game
-# print("\nScore: " + str(score))  # Prints the score on game end
+f.write("\n" + str(score))
+f.close()
+print("\nFinal Score: " + str(score))  # Prints the score on game end
